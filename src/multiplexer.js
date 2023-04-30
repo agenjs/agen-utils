359e9b6f68f10095a92db0dd3e0fc796b58b0d89
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-04-30 13:38:35 +0200

    refactor: update multiplexer implementation

--- src/multiplexer.js
+++ src/multiplexer.js
@@ -1,27 +1,37 @@
 import iterator from "./iterator.js";
 import listen from "./listen.js";
 
-export default function multiplexer(it, newQueue) {
-  let list = [], complete;
-  return async function* () {
-    if (!list) throw new Error("Iterations finished");
-    yield* iterator((observer) => {
-      if (list.length === 0) {
-        const forAll = async (action) => Promise.all(list.map(action));
-        complete = listen(it, {
-          next: async (v) => await forAll((o) => o.next(v)),
-          complete: async () => await forAll((o) => o.complete()),
-          error: async (e) => await forAll((o) => o.error(e)),
-        });
-      }
-      list.push(observer);
-      return () => {
-        list = list.filter((o) => observer !== o);
-        if (list.length === 0) {
-          list = null;
-          complete();
-        }
-      };
-    }, newQueue)();
-  };
+export default function multiplexer(it, newQueue, awaitNew) {
+  let list = [],
+    value,
+    error,
+    started = false,
+    done = false;
+  const forAll = async (action) => Promise.all(list.map(action));
+  listen(it, {
+    next: async (v) => {
+      started = true;
+      value = v;
+      await forAll((o) => o.next(v));
+    },
+    complete: async () => {
+      done = true;
+      await forAll((o) => o.complete());
+    },
+    error: async (e) => {
+      error = e;
+      done = true;
+      await forAll((o) => o.error(e));
+    },
+  });
+  return iterator((observer) => {
+    if (done) {
+      return error ? observer.error(error) : observer.complete();
+    }
+    list.push(observer);
+    started && !awaitNew && observer.next(value);
+    return () => {
+      list = list.filter((o) => observer !== o);
+    };
+  }, newQueue);
 }
