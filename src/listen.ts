864eba36443bf0c58a59f93acf494a89d7656d4c
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2024-12-11 08:35:18 +0100

    refactor: add finalization for iterator listeners

--- src/listen.ts
+++ src/listen.ts
@@ -7,19 +7,25 @@ import {
 
 export function listen<T, E = Error>(
   params: IterableLike<T> | (() => IterableLike<T>),
-  observer: ObserverLike<T, E>
+  observer: ObserverLike<T, E>,
 ): () => void {
   let it: AsyncGenerator<T, void>;
   (async () => {
     const o = toObserver<T, E>(observer);
+    let error: E | undefined;
     try {
       it = toAsyncIterator<T>(params);
       for await (const value of it) {
         await o.next(value);
       }
-      o.complete && (await o.complete());
-    } catch (error) {
-      o.error && (await o.error(error as E));
+    } catch (e) {
+      error = e as E;
+    } finally {
+      if (error) {
+        o.error && (await o.error(error));
+      } else {
+        o.complete && (await o.complete());
+      }
     }
   })();
   return () => {
